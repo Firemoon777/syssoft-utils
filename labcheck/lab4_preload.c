bb79f99d490b31a2d050a9abb21fb8f3c78db670
Firemoon777 <firemoon@icloud.com>
2017-02-04 17:10:05 +0300

    clean up

--- labcheck/lab4_preload.c
+++ labcheck/lab4_preload.c
@@ -1,34 +1,35 @@
 #include <unistd.h>
-#include "../labcheck.h"
+
+#define EXITCODE 42
 
 int scanf(const char *format, ...) {
-	_exit(LABCHECK_RESTRICTED_OP_EXITCODE);
+	_exit(EXITCODE);
 }
 
 /*int fscanf(FILE *stream, const char *format, ...) {
-	_exit(LABCHECK_RESTRICTED_OP_EXITCODE);
+	_exit(EXITCODE);
 }
 
 int sscanf(const char *str, const char *format, ...) {
-	_exit(LABCHECK_RESTRICTED_OP_EXITCODE);
+	_exit(EXITCODE);
 }*/
 
 int printf(const char *format, ...) {
-	_exit(LABCHECK_RESTRICTED_OP_EXITCODE);
+	_exit(EXITCODE);
 }
 
 /*int fprintf(FILE *stream, const char *format, ...) {
-	exit(LABCHECK_RESTRICTED_OP_EXITCODE);
+	exit(EXITCODE);
 }
 
 int dprintf(int fd, const char *format, ...) {
-	exit(LABCHECK_RESTRICTED_OP_EXITCODE);
+	exit(EXITCODE);
 }
 
 int sprintf(char *str, const char *format, ...) {
-	exit(LABCHECK_RESTRICTED_OP_EXITCODE);
+	exit(EXITCODE);
 }
 
 int snprintf(char *str, size_t size, const char *format, ...) {
-	exit(LABCHECK_RESTRICTED_OP_EXITCODE);
+	exit(EXITCODE);
 }*/
