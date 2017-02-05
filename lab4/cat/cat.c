0c762e0da0860c6220d675b2349a574f627841d1
Firemoon777 <firemoon@icloud.com>
2017-02-05 19:58:19 +0300

    splint verification done

--- lab4/cat/cat.c
+++ lab4/cat/cat.c
@@ -10,30 +10,34 @@
 
 #define BUFFER_SIZE 80
 
-int main(int argc, char** argv) {
+static void do_work(const char* filename) {
 	char buff[BUFFER_SIZE+1];
 	int fd;
 	ssize_t buff_readed;
-	size_t i = 1;
+	if(strcmp(filename, "-") == 0) {
+		fd = STDIN_FILENO;
+	} else {
+		fd = open(filename, O_RDONLY);
+	}
+	if(fd < 0) {
+		perror(filename);	
+		return;
+	}
+	memset(buff, 0, BUFFER_SIZE+1);
+	while((buff_readed = read(fd, buff, BUFFER_SIZE)) > 0) {
+		(void)write(STDOUT_FILENO, buff, (size_t)buff_readed);
+		memset(buff, 0, BUFFER_SIZE);
+	}
+	(void)close(fd);
+}
+
+int main(int argc, char** argv) {
+	int i = 1;
 	if(argc == 1) {
-		argc++;
-		*(argv + 1) = "-\0";
+		do_work("-");
 	}
 	for(i = 1; i < argc; i++) {
-		if(strcmp(argv[i], "-") == 0) {
-			fd = STDIN_FILENO;
-		} else {
-			fd = open(argv[i], O_RDONLY);
-		}
-		if(fd < 0) {
-			perror(argv[i]);	
-			continue;
-		}
-		memset(buff, 0, BUFFER_SIZE+1);
-		while((buff_readed = read(fd, buff, BUFFER_SIZE)) > 0) {
-			write(STDOUT_FILENO, buff, buff_readed);
-			memset(buff, 0, BUFFER_SIZE);
-		}
+		do_work(argv[i]);
 	}
 	return 0;
 }
