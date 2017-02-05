3464a2b233ce7cbd0dd2bec670e5d16f41dee2aa
Firemoon777 <firemoon@icloud.com>
2017-02-05 07:49:39 +0300

    new version cp

--- lab4/cp/cp.c
+++ lab4/cp/cp.c
@@ -13,35 +13,39 @@
 int main(int argc, char** argv) {
 	char buff[BUFFER_SIZE+1];
 	int fd_in, fd_out;
-	ssize_t buff_readed;
-	//size_t i;
+	ssize_t buff_readed, buff_w;
 	if(argc != 3) {
-		err_print(argv[0], "Insufficient arguments\n", "Must be: src dest\n");
+		write(STDOUT_FILENO, "Usage: cp src dest\n", 19);  
 		return 1;
 	}
+	
 	fd_in = open(argv[1], O_RDONLY);
 	if(fd_in < 0) {
-		err_parse(errno, argv[0], argv[1]);
+		perror(argv[1]);
 		return errno;
 	}
+	
 	struct stat fileStat;
     if(stat(argv[1], &fileStat) < 0) { 
 		return 3;
 	}    
-	fileStat.st_mode = fileStat.st_mode & (S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
-	fd_out = open(argv[2], O_RDONLY);
-	if(fd_out > 0) {
-		err_parse(EEXIST, argv[0], argv[2]);
-		return 2;
-	}
-	close(fd_out);
-	fd_out = open(argv[2], O_CREAT | O_WRONLY, fileStat.st_mode);
+	fileStat.st_mode = fileStat.st_mode;
+	
+	fd_out = open(argv[2], O_CREAT | O_WRONLY | O_EXCL, fileStat.st_mode);
 	if(fd_out < 0) {
-		err_parse(errno, argv[0], argv[2]);
+		perror(argv[2]);
 		return errno;
 	}
 	while((buff_readed = read(fd_in, buff, BUFFER_SIZE)) > 0) {
-		write(fd_out, buff, buff_readed);
+		if(buff_readed < 0) {
+			perror("read");
+			return errno;
+		}
+		buff_w = write(fd_out, buff, buff_readed);
+		if(buff_w < 0) {
+			perror("write");
+			return errno;
+		}
 	}
 	return 0;
 }
