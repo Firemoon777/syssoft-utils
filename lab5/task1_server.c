3bd5499d25514fbc7c3e5d78ada6f7b2e7eedf64
Voldemar <firemoon@icloud.com>
2017-04-17 07:57:57 +0300

    shmremove

--- lab5/task1_server.c
+++ lab5/task1_server.c
@@ -9,9 +9,18 @@
 #include <time.h>
 #include <sys/loadavg.h>  /* getloadavg */
 #include <string.h>       /* memset */
+#include <signal.h>
 
 #include "task.h"
 
+key_t key;
+int mem_id;
+
+void sighandler(int signo) {
+	(void)signo;
+	shmctl(mem_id, IPC_RMID, NULL);	
+	_exit(0);	
+}
 
 struct server_info save_info(void) {
 	struct server_info info;
@@ -28,8 +37,11 @@ int main(void) {
 	time_t start_time = time(NULL);
 
 	time_t cur_time;
-	key_t key;
-    int mem_id;
+
+	struct sigaction act;
+	memset(&act, 0, sizeof(struct sigaction));
+	act.sa_handler = sighandler;
+	sigaction(SIGINT, &act, NULL);
 
 	/* create ipc file */
 	close(open(name, O_RDWR | O_CREAT, 0644));
